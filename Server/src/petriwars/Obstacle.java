005af2c6b277d9925e39ceb5d28fa4b8abf10eff
fuzzymillipede <fuzzymillipede@gmail.com>
2011-05-31 17:56:08 -0700

    Edited Server/src/petriwars/Obstacle.java via GitHub

--- Server/src/petriwars/Obstacle.java
+++ Server/src/petriwars/Obstacle.java
@@ -3,6 +3,32 @@ package petriwars;
 import java.util.ArrayList;
 
 public class Obstacle {
-	private ArrayList<Point> corners;
+private ArrayList<Square> squares;
+	private ArrayList<Square> corners;
+
+public Obstacle(byte[][] map_raw, Square[][] map, int x, int y)
+{
+squares = new ArrayList<Square>();
+corners = new ArrayList<Square>();
+grow(map_raw, map, x, y);
+}
+
+public void grow(byte[][] map_raw, Square[][] map, int x, int y)
+{
+squares.add(map[y][x]);
+map[y][x].addObstacle(this);
+if (x < map[0].length - 1 && map_raw[y][x + 1] == OBSTACLE)
+grow(map_raw, map, x + 1, y);
+
+
+if (y < map.length - 1)
+{
+if (map_raw[y + 1][x] == OBSTACLE)
+grow(map_raw, map, x, y + 1);
+if (x > 0 && map_raw[y + 1][x - 1] == OBSTACLE)
+grow(map_raw, map, x - 1, y + 1);
+if (x < map[0].length - 1 && map_raw[y + 1][x + 1] == OBSTACLE)
+grow(map_raw, map, x + 1, y + 1);
+}
 	
 }
