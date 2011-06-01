23e0f2dedf317ac11dd99ffef2fb433e9cfcace8
fuzzymillipede <fuzzymillipede@gmail.com>
2011-05-31 17:02:00 -0700

    Edited Server/src/petriwars/types/Map.java via GitHub

--- Server/src/petriwars/types/Map.java
+++ Server/src/petriwars/types/Map.java
@@ -13,11 +13,21 @@ public class Map {
 	private int height;
 private int width;
 	public Map(String url){
-		try {
-			if(get_map_from_file(url)==false){}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}											//something is wrong with the map
+		File map_file = new File(url);
+map_name = map_file.getName();
+byte[][] map_raw = FileParser.getByteMap(map_file);
+
+height = map_raw.length;
+width = map_raw[0].length;
+map = new Square[y][x];
+for (int y = 0; y < height; y++) 
+for (int x = 0; x < width; x++) 
+map[y][x] = new Square(map_raw[y][x]);
+
+for (int y = 0; y < height; y++) 
+for (int x = 0; x < width; x++) 
+if (map_raw[y][x] == OBSTACLE && map[y][x].obstacle == NULL)
+map[y][x].obstacle = new Obstacle(map_raw, map, x, y);
 		
 	}
 
@@ -47,23 +57,4 @@ return map[y][x].getObstacle();
 		map[col][row]='1';
 	}
 	
-	private boolean get_map_from_file(String url) throws IOException{
-File map_file = new File(url);
-map_name = map_file.getName();
-byte[][] map_raw = FileParser.getByteMap(map_file);
-
-height = map_raw.length;
-width = map_raw[0].length;
-map = new Square[y][x];
-for (int y = 0; y < height; y++) 
-for (int x = 0; x < width; x++) 
-map[y][x] = new Square(map_raw[y][x]);
-
-for (int y = 0; y < height; y++) 
-for (int x = 0; x < width; x++) 
-if (map_raw[y][x] == OBSTACLE && map[y][x].obstacle == NULL)
-map[y][x].obstacle = new Obstacle(map_raw, map, x, y);
-
-return true;
-	}
 }
