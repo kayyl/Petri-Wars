055b445ed1f6867c6dd93e37ee46fcb18654058f
Adam Park <fuzzymillipede@gmail.com>
2011-06-07 21:56:11 -0400

    i worked on the obstacle and corners

---
+++ Server/src/petriwars/FileParser.java~
@@ -0,0 +1,100 @@
+package petriwars;
+
+import java.io.FileInputStream;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class FileParser
+{
+    public static void main(String[] args) throws Exception
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter name of map file: ");
+        String name = s.nextLine();
+        s.close();
+        if (name.equals(""))
+            name = "map_test.bmp";
+        File f = new File(name);
+                
+        printFile(f);
+    }
+    
+    public static byte[][] getByteMap(File f) throws Exception
+    {
+        byte[] contents = new byte[(int)f.length()];
+        FileInputStream in = new FileInputStream(f);
+        in.read(contents);
+        in.close();
+     
+        int width = getIntVal(0x12, contents);
+        //width = width + 3 & -4;
+        int pad = (width + 3 & -4) - width;
+        //System.out.println(width + " " + pad);
+        int length = getIntVal(0x16, contents);
+        //System.out.println(length);
+        
+        byte[][] mapBytes = new byte[length][width];
+        
+        int pos = 1078;
+        for (int i = length - 1; i >= 0; i--)
+        {
+            for (int i2 = 0; i2 < mapBytes[i].length; i2++)
+                mapBytes[i][i2] = contents[pos++];
+            pos += pad;
+        }    
+        return mapBytes;        
+    }
+    
+    public static void printFile(File f) throws Exception
+    {
+        byte[] contents = new byte[(int)f.length()];
+        FileInputStream in = new FileInputStream(f);
+        in.read(contents);
+        in.close();
+        
+        char[] mapVals = new char[256];
+        mapVals[0x00] = '#';
+        mapVals[0xF9] = '.';
+        mapVals[0xFF] = ' ';
+        mapVals[0xFC] = 'N';
+        mapVals[0xFA] = 'S';
+        mapVals[0xFB] = 'G';
+        
+        int width = getIntVal(0x12, contents);
+        //width = width + 3 & -4;
+        int pad = (width + 3 & -4) - width;
+        //System.out.println(width + " " + pad);
+        int length = getIntVal(0x16, contents);
+        //System.out.println(length);
+        
+        char[][] mapText = new char[length][width];
+        
+        int pos = 1078;
+        for (int i = length - 1; i >= 0; i--)
+        {
+            for (int i2 = 0; i2 < mapText[i].length; i2++)
+                mapText[i][i2] = mapVals[contents[pos++] & 255];
+            pos += pad;
+        }
+        
+        PrintWriter out = new PrintWriter(new File("output.txt"));
+        for (int i = 0; i < mapText.length; i++)
+        {
+            for (int i2 = 0; i2 < mapText[i].length; i2++)
+            {
+                out.print(mapText[i][i2]);
+            }
+            out.println();
+        }
+        out.close();
+    }
+    
+    public static int getIntVal(int startAddress, byte[] b)
+    {
+        return (b[startAddress + 3] << 24)
+            + ((b[startAddress + 2] & 0xFF) << 16)
+            + ((b[startAddress + 1] & 0xFF) << 8)
+            + (b[startAddress] & 0xFF);
+    }//end getIntVal
+}
--- Server/src/petriwars/types/Obstacle.java
+++ Server/src/petriwars/types/Obstacle.java
@@ -2,35 +2,94 @@ package petriwars.types;
 
 import java.util.ArrayList;
 
-
 public class Obstacle {
-	private final byte OBSTACLE = 0x00;
-	private ArrayList<Square> squares;
-	private ArrayList<Square> corners;
+    private final byte OBSTACLE = 0x00;
+    private ArrayList<Square> squares;
+    private ArrayList<Points> corners;
+    
+    public Obstacle(byte[][] map_raw, Square[][] map, int x, int y)
+    {
+        squares = new ArrayList<Square>();
+        corners = new ArrayList<Points>();
+        grow(map_raw, map, x, y);
+    }
+    
+    public void grow(byte[][] map_raw, Square[][] map, int x, int y)
+    {
+        squares.add(map[y][x]);
+        map[y][x].addObstacle(this);
+        int w = map[0].length;
+        int h = map.length;
+        int nw, wn, ne, en, se, es, sw, ws;
 
-	public Obstacle(byte[][] map_raw, Square[][] map, int x, int y)
-	{
-		squares = new ArrayList<Square>();
-		corners = new ArrayList<Square>();
-		grow(map_raw, map, x, y);
-	}
-
-	public void grow(byte[][] map_raw, Square[][] map, int x, int y)
-	{
-		squares.add(map[y][x]);
-		map[y][x].addObstacle(this);
-		if (x < map[0].length - 1 && map_raw[y][x + 1] == OBSTACLE)
-			grow(map_raw, map, x + 1, y);
-
-
-		if (y < map.length - 1)
-		{
-			if (map_raw[y + 1][x] == OBSTACLE)
-				grow(map_raw, map, x, y + 1);
-			if (x > 0 && map_raw[y + 1][x - 1] == OBSTACLE)
-				grow(map_raw, map, x - 1, y + 1);
-			if (x < map[0].length - 1 && map_raw[y + 1][x + 1] == OBSTACLE)
-				grow(map_raw, map, x + 1, y + 1);
-		}
-
-	}
+        if (isEmpty(x - 1, y - 1, w, h, map_raw, map))
+        {
+            nw++;
+            wn++;
+        }
+        if (isEmpty(x, y - 1, w, h, map_raw, map))
+        {
+            nw++;
+            wn++; 
+            ne++;
+            en++;
+        }
+        if (isEmpty(x + 1, y - 1, w, h, map_raw, map))
+        {
+            ne++;
+            en++;
+        }
+        if (isEmpty(x - 1, y, w, h, map_raw, map))
+        {
+            sw++;
+            ws++;
+            ne++;
+            en++;
+        }
+        if (isEmpty(x + 1, y, w, h, map_raw, map))
+        {
+            se++;
+            es++;
+            ne++;
+            en++;
+        }
+        if (isEmpty(x - 1, y + 1, w, h, map_raw, map))
+        {
+            sw++;
+            ws++;
+        }
+        if (isEmpty(x, y + 1, w, h, map_raw, map))
+        {
+            se++;
+            es++;
+            sw++;
+            ws++;
+        }
+        if (isEmpty(x + 1, y + 1, w, h, map_raw, map))
+        {
+            se++;
+            es++;
+        }
+        
+        if (nw == 4 || wn == 4)
+            corners.add(new Point(x - 0.5, y - 0.5));
+        if (ne == 4 || en == 4)
+            corners.add(new Point(x + 0.5, y - 0.5));
+        if (sw == 4 || ws == 4)
+            corners.add(new Point(x - 0.5, y + 0.5));
+        if (se == 4 || es == 4)
+            corners.add(new Point(x + 0.5, y + 0.5));
+    }
+    
+    private boolean isEmpty(int x, int y, int w, int h, byte[][] m, Square[][] m2)
+    {
+        if (x < 0 || y < 0 || x >= w || y >= h)
+            return false;
+        if ([y][x] == OBSTACLE)
+        {
+            grow(m, m2, x, y);
+            return false;
+        }
+        return true;
+    }
+}
